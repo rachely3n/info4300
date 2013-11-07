63f2f5d8a0c9bb097904e8a5fad6468b1b94bae9
rachely3n <ry88@cornell.edu>
2013-11-07 18:10:35 -0500

    starting part A

---
+++ prog_p2/src/GetZipf.java
@@ -0,0 +1,15 @@
+import org.apache.lucene.analysis.Tokenizer;
+
+public class GetZipf {
+	public static void main(String[] args){ //run errythang
+		
+	}
+//get all documents
+
+//tokenize all documents
+	
+	//don't filter stop words
+	//retrieve frequencies for each word
+	
+	
+}
