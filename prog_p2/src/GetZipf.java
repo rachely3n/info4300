c09aa366880df2eab0b749c98ee579bcf1a545ac
rachely3n <ry88@cornell.edu>
2013-11-07 18:31:00 -0500

    still figuring stuff out

--- prog_p2/src/GetZipf.java
+++ prog_p2/src/GetZipf.java
@@ -1,4 +1,6 @@
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+
 
 public class GetZipf {
 	public static void main(String[] args){ //run errythang
@@ -7,6 +9,21 @@ public class GetZipf {
 //get all documents
 
 //tokenize all documents
+	 
+	/**
+	 *
+	 *  
+StandardTokenizer src = new StandardTokenizer(YOUR_VERSION, YOUR_FILE_READER);
+src.setMaxTokenLength(YOUR_MAX_LENGTH);
+TokenStream tokenStream = new StandardFilter(Your_VERSION, src);
+CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+
+tokenStream.reset();
+while (tokenStream.incrementToken()) {
+   String term = charTermAttribute.toString(); // here you get the terms
+}
+	 */
+
 	
 	//don't filter stop words
 	//retrieve frequencies for each word
