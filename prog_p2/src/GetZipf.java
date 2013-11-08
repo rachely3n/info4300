671b080bc87ac2cc320863eaa2689f886de704ee
rachely3n <ry88@cornell.edu>
2013-11-07 19:43:31 -0500

    getting closer, just got to figure out how to tokenize properly then hash words to get frequencies

--- prog_p2/src/GetZipf.java
+++ prog_p2/src/GetZipf.java
@@ -1,12 +1,42 @@
-import org.apache.lucene.analysis.Tokenizer;
+//import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 
+import java.util.*;
+import java.io.*;
+import org.apache.lucene.util.Version;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
 
 public class GetZipf {
-	public static void main(String[] args){ //run errythang
-		
+	//public static final Version LUCENE_40;
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException{ //run errythang
+		//get all documents
+		String docsDirPath = "data/txt"; // directory containing documents
+		//filereader extends inputstreamreader
+		FileReader fread;
+		StandardTokenizer tok;
+		File docDir = new File(docsDirPath);
+		for (File f : docDir.listFiles()) { //read each file from directory
+			fread = new FileReader(f);
+			//text tokenization
+			//what is Your_versioN? @.@ lol
+			tok = new StandardTokenizer(Version.LUCENE_40,fread);
+			//set max token length? 
+			TokenStream tokenStream = new LowerCaseFilter(Version.LUCENE_40,tok);
+			CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+			
+			tokenStream.reset();
+			while(tokenStream.incrementToken()){//while token exists
+				String term = charTermAttribute.toString();
+			}
+			fread.close();
+			
+		}
 	}
-//get all documents
+
 
 //tokenize all documents
 	 
