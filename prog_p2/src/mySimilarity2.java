09a797d8dec340b184a91f9ec337e47054b499d7
jpf228 <jpf228@cornell.edu>
2013-11-08 02:05:31 -0500

    Update mySimilarity2.java

--- prog_p2/src/mySimilarity2.java
+++ prog_p2/src/mySimilarity2.java
@@ -1,1 +1,16 @@
-public
+//Secondary version used so we can have two different tf.idf scoring measures as the project asks for.
+public class mySimilarity extends TFIDFSimilarity {
+  public mySimilarity() {
+    super();
+  }
+  
+  public abstract float tf(float freq) {
+    float val = log(freq + 1);
+    return val;
+  }
+  
+  public abstract float idf(long docFreq, long numDocs) {
+    float val = log(numDocs / (docFreq + 1)) + 1;
+    return val;
+  }
+}
